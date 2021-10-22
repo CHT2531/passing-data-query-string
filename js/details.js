9481b6a39c2d824f09fa2aede47dc803941e08e4
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-10-22 16:35:49 +0100

    Add files via upload

---
+++ js/details.js
@@ -0,0 +1,33 @@
+function loadData(url,callback)
+{
+	fetch(url).then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		callback(json)
+	});
+} //end of loadData()
+
+
+
+
+function populateContent(country)
+{
+	const titleEl=document.getElementById("title");
+	const capitalEl=document.getElementById("capital");
+	const populationEl=document.getElementById("population");
+	titleEl.textContent = country.name;
+	capitalEl.textContent = country.capital;
+	populationEl.textContent = country.population;
+}
+
+
+function init(){
+	//URLSearchParams provides an easy method for getting data from the querystring e.g. details.html?id=3
+	//see https://davidwalsh.name/query-string-javascript for more info
+	const urlParams = new URLSearchParams(window.location.search);
+	const id = urlParams.get("id");
+	loadData("data/country"+id+".json",populateContent); //request a JSON file e.g. country3.json
+}
+
+
+init();
